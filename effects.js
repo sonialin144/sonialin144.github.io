3abeda57361d0052b39b9125f164293344da9cf0
Sonia Lin <sonia.lin.144@gmail.com>
2023-12-08 11:15:09 -0500

    fix link

--- effects.js
+++ effects.js
@@ -81,7 +81,7 @@ function reveal(){
     document.querySelector(".plant").addEventListener("click", ()=>{window.location = "plant.html";})
     document.querySelector(".claws").addEventListener("click", ()=>{window.location = "https://claws.engin.umich.edu/";})
     document.querySelector(".vocab").addEventListener("click", ()=>{window.location = "vocab.html";})
-    document.querySelector(".vocab").addEventListener("click", ()=>{window.location = "niantic.html";})
+    document.querySelector(".niantic").addEventListener("click", ()=>{window.location = "niantic.html";})
     //deployment test
 
 });
